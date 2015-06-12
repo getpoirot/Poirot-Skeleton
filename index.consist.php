5b76a83c4ea3c23bd042bcdcaf151b8d172dfc04
Payam Naderi <naderi.payam@gmail.com>
2015-06-12 21:41:02 +0430

    code cleanup

--- index.consist.php
+++ index.consist.php
@@ -9,14 +9,15 @@
 define('REQUEST_MICROTIME', microtime(true));
 define('DS', DIRECTORY_SEPARATOR);
 
-define('APP_DIR_ROOT'       , dirname(__FILE__) );
-define('APP_DIR_APPLICATION', APP_DIR_ROOT .DS. '_app');
-define('APP_DIR_LIBRARIES',   	APP_DIR_APPLICATION .DS. 'vendor');
-define('APP_DIR_MODULES', 		APP_DIR_APPLICATION .DS. 'vendor');
+define('APP_DIR_WWW'        , dirname(__FILE__) );
+// by default application folder is in www public
+// it can be changed to any other folder like APP_DIR_WWW.'/../app-folder'
+define('APP_DIR_APPLICATION', APP_DIR_WWW .DS. '_app');
+define('APP_DIR_VENDOR',   	APP_DIR_APPLICATION .DS. 'vendor');
 define('APP_DIR_CORE', 			APP_DIR_APPLICATION .DS. 'modules');
 define('APP_DIR_CONFIG', 		APP_DIR_APPLICATION .DS. 'config');
 define('APP_DIR_TEMP', 			APP_DIR_APPLICATION .DS. 'tmp');
 
 // Setup autoLoading
-if (file_exists(APP_DIR_LIBRARIES .DS. 'autoload.php'))
-    require_once APP_DIR_LIBRARIES .DS. 'autoload.php';
+if (file_exists(APP_DIR_VENDOR .DS. 'autoload.php'))
+    require_once APP_DIR_VENDOR .DS. 'autoload.php';
--- index.php
+++ index.php
@@ -4,21 +4,22 @@ namespace
     (!defined('PHP_VERSION_ID') or PHP_VERSION_ID < 50306 ) and
     exit('Needs at least PHP5.3; youre current php version is ' . phpversion() . '.');
 
-    /**
+    /*
+     * Application Consistencies and AutoLoad
+     */
+    require 'index.consist.php';
+    (APP_DIR_WWW !== dirname(__FILE__))
+        and die('put index.consist file beside '.basename(__FILE__));
+
+    /*
      * This makes our life easier when dealing with paths. Everything is relative
      * to the application root now.
      */
     chdir(__DIR__);
 
-    /**
-     * Application Consistencies and AutoLoad
-     *
-     */
-    require 'index.consist.php';
-
     // Run the application!
     try {
-
+        
     }
     catch (Exception $e) {
         throw $e;
