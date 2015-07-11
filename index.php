f4b9c47e34205131738a0057b3da57f4ce55c896
Payam Naderi <naderi.payam@gmail.com>
2015-07-11 19:05:06 +0430

    code cleanup

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
 namespace
 {
+    use Poirot\Application\Sapi;
+
     (!defined('PHP_VERSION_ID') or PHP_VERSION_ID < 50306 ) and
     exit('Needs at least PHP5.3; your current php version is ' . phpversion() . '.');
 
     /*
      * Application Consistencies and AutoLoad
      */
-    require 'index.consist.php';
+    require_once 'index.consist.php';
 
     /*
      * change cwd to the application root by default
@@ -15,10 +17,13 @@ namespace
     chdir(__DIR__);
 
     // Run the application!
+    $app = new Sapi();
     try {
+        $app->run();
 
-    }
-    catch (Exception $e) {
+    } catch (Exception $e) {
         throw $e;
     }
+
+    die(); // every soul shall taste of death
 }
