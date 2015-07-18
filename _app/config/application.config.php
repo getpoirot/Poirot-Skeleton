94a84d22f84320668ff32c6ef104a4676fb92837
Payam Naderi <naderi.payam@gmail.com>
2015-07-18 23:34:58 +0430

    initialize application config

--- _app/config/application.config.php
+++ _app/config/application.config.php
@@ -1,9 +1,26 @@
 <?php
+/**
+ * Options pass to Application as SetterBuilder
+ *
+ * @see \Poirot\Application\Sapi::__construct
+ */
 return [
-    'module' => [
-        'enabled' => [
-            'application',
-            'analytics',
+    'modules' =>
+        # module options
+        [
+            'enabled' => [
+                'application',
+                'analytics',
+            ],
         ],
-    ],
+
+    // set default container service
+    'container' =>
+        /**
+         * container builder options
+         * @see \Poirot\Container\ContainerBuilder
+         */
+        [
+
+        ],
 ];
