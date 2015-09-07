68a1fc8b95521694edf56ef0da56202c2d609f19
Payam Naderi <naderi.payam@gmail.com>
2015-09-07 23:07:39 +0430

    init default theme folder

--- index.consist.php
+++ index.consist.php
@@ -9,17 +9,18 @@
 // Define Consts:
 define('REQUEST_MICROTIME', microtime(true));
 
-define('APP_DIR_WWW'        , dirname(__FILE__) );
+define('APP_DIR_WWW'          , dirname(__FILE__) );
+define('APP_DIR_THEME_DEFAULT', APP_DIR_WWW.'/theme' );
 # by default application folder is in www public
 # it can be changed to any other folder like APP_DIR_WWW.'/../app-folder'
-define('APP_DIR_APPLICATION', APP_DIR_WWW.'/_app');
-define('APP_DIR_VENDOR',   	     APP_DIR_APPLICATION.'/vendor');
-define('APP_DIR_CORE', 			    APP_DIR_APPLICATION.'/modules');
-define('APP_DIR_CONFIG', 		    APP_DIR_APPLICATION.'/config');
-define('APP_DIR_TEMP', 			    APP_DIR_APPLICATION.'/tmp');
+define('APP_DIR_APPLICATION',  APP_DIR_WWW.'/_app');
+define('APP_DIR_VENDOR',   	      APP_DIR_APPLICATION.'/vendor');
+define('APP_DIR_CORE', 			     APP_DIR_APPLICATION.'/modules');
+define('APP_DIR_CONFIG', 		     APP_DIR_APPLICATION.'/config');
+define('APP_DIR_TEMP', 			     APP_DIR_APPLICATION.'/tmp');
 
 // Setup autoLoading:
 if (file_exists(APP_DIR_VENDOR.'/autoload.php'))
     require_once APP_DIR_VENDOR.'/autoload.php';
 
-## (!) Don't add something on below lines
+## (!) Don't add something on lines below
