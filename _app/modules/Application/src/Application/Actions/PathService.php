c8975de750dadee4f97853edbc7bb9df30c73a9b
Payam Naderi <naderi.payam@gmail.com>
2015-09-17 00:57:44 +0430

    default serverUrl variable for http request.

--- _app/modules/Application/src/Application/Actions/PathService.php
+++ _app/modules/Application/src/Application/Actions/PathService.php
@@ -2,6 +2,7 @@
 namespace Application\Actions;
 
 use Poirot\Container\Service\AbstractService;
+use Poirot\Http\Interfaces\Message\iHttpRequest;
 
 class PathService extends AbstractService
 {
@@ -17,7 +18,28 @@ class PathService extends AbstractService
      */
     function createService()
     {
-        return new PathAction;
+        $pathAction = new PathAction;
+
+        # register default paths and variables
+        if ($this->services()->from('/')->get('request') instanceof iHttpRequest) {
+            $pathAction->params()->set('serverUrl', $this->__getServerUrl());
+        }
+
+        return $pathAction;
+    }
+
+    protected function __getServerUrl()
+    {
+        /** @var \Poirot\Http\Message\HttpRequest $request */
+        $request = $this->services()->from('/')->get('Request');
+        $uri     = $request->getUri();
+
+        $server  = '';
+        (!$scheme = $uri->getScheme()) ?: $server .= $scheme.'://';
+        (!$host   = $uri->getHost())   ?: $server .= $host;
+        (!$port   = $uri->getPort())   ?: $server .= ':'.$port;
+
+        return $server;
     }
 }
  
