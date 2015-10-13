03a7d4b6507771463b1df2b16f5959db635e3ae4
Payam Naderi <naderi.payam@gmail.com>
2015-10-13 15:34:59 +0330

    implement application default configs

--- _app/modules/Application/src/Application/Module.php
+++ _app/modules/Application/src/Application/Module.php
@@ -4,12 +4,14 @@ namespace Application;
 use Application\Actions\ApplicationActionsBuilder;
 use Application\Actions\Helper\PathAction;
 use Application\HttpSapi\ViewModelRenderer;
+use Poirot\Application\Config;
 use Poirot\Application\Interfaces\iApplication;
 use Poirot\Application\Interfaces\Sapi\iSapiModule;
 use Poirot\Application\Sapi;
 use Poirot\Application\Sapi\Module\ModuleActionsContainer;
 use Poirot\Container\Container;
 use Poirot\Container\Service\InstanceService;
+use Poirot\Core\Interfaces\iDataSetConveyor;
 use Poirot\Loader\Autoloader\AggregateAutoloader;
 use Poirot\Router\Http\RChainStack;
 
@@ -19,6 +21,7 @@ class Module implements iSapiModule
     , Sapi\Module\Feature\ServiceContainerFeature
     , Sapi\Module\Feature\ActionProviderFeature
     , Sapi\Module\Feature\ResolveToServicesFeature
+    , Sapi\Module\Feature\ConfigFeature
 {
     /**
      * Init Module Against Application
@@ -72,6 +75,37 @@ class Module implements iSapiModule
         ## replace default renderer with Application renderer including stuffs
         if ($services->has('ViewModelRenderer'))
             $services->set(new InstanceService('ViewModelRenderer', new ViewModelRenderer));
+    }
+
+    /**
+     * Register config key/value
+     *
+     * - you may return an array or iDataSetConveyor
+     *   that would be merge with config current data
+     *
+     * @param Config $config
+     *
+     * @return array|iDataSetConveyor
+     */
+    function withConfig(Config $config)
+    {
+        return [
+            'view_renderer' => [
+                /** @see ViewRenderStrategy::getDefaultLayout */
+                'default_layout'   => 'default',
+
+                /** @see onErrorListener::__invoke */
+                'error_view_template' => [
+                    ## full name of class exception
+
+                    ## use null on second index cause view template render as final layout
+                    ## 'Exception' => ['error/error', null],
+
+                    'Exception' => ['error/error', 'blank'],
+                    'Poirot\Application\Exception\RouteNotMatchException' => 'error/404',
+                ],
+            ],
+        ];
     }
 
     /**
