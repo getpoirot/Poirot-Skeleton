1cd2f07c4f622b732db33451e8124254415338d6
Payam Naderi <naderi.payam@gmail.com>
2016-05-25 23:08:12 +0430

    code cleanup

--- src/config/services.conf.php
+++ src/config/services.conf.php
@@ -7,30 +7,30 @@ return array(
     'implementations' =>
      // services interface implementation contract
         array(
-            'config' => '\Poirot\Std\Interfaces\Struct\iDataEntity',
-            #'config' => \Poirot\Std\Interfaces\Struct\iDataEntity::class,
-
             'sapi'   => '\Poirot\Application\Interfaces\iApplication',
             #'sapi'   => \Poirot\Application\Interfaces\iApplication::class,
+
+            'config' => '\Poirot\Std\Interfaces\Struct\iDataEntity',
+            #'config' => \Poirot\Std\Interfaces\Struct\iDataEntity::class,
         ),
     'services' =>
         array(
             'sapi' =>
             // Application Sapi Service Factory
                 array(
-                    ':class' => 'Poirot\Application\SapiService',
-                    #':class' => \Poirot\Application\SapiService::class ,
-                    'config' => 'sapi.settings'
+                    ':class'  => 'Poirot\Application\SapiService',
+                    #':class'  => \Poirot\Application\SapiService::class ,
+                    'setting' => 'sapi.setting'
                     // config can be (string) as registered service
                     // or \Traversable|array instance
                 ),
-            'sapi.settings' =>
+            'sapi.setting' =>
             // Implement of \Traversable|array, defined as service so it can be
             // replaced with other to load config from DB in exp.
                 array(
-                    ':class'      => 'Poirot\Application\ConfigService',
-                    #':class'      => \Poirot\Application\ConfigService::class,
-                    'sapi_config' => \Poirot\Config\load(PT_DIR_CONFIG.'/sapi'),
+                    ':class'  => 'Poirot\Application\ServiceConfigSapiDefault',
+                    #':class'  => \Poirot\Application\ServiceConfigSapiDefault::class,
+                    'setting' => \Poirot\Config\load(PT_DIR_CONFIG.'/sapi'),
                 ),
         ),
 );
