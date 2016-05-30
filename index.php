28b11cfa98dd7601eb0835e108ed80799b83abcb
Payam Naderi <naderi.payam@gmail.com>
2016-05-30 10:53:42 +0430

    code cleanup

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ namespace Poirot
 
     // Run the application:
     P\Std\ErrorStack::handleError(E_ERROR|E_RECOVERABLE_ERROR|E_USER_ERROR, function($error) { throw $error; });
-    P\Std\ErrorStack::handleException(function ($error) { printException($error); });
+    P\Std\ErrorStack::handleException(function ($error) { P\printException($error); });
 
     # start application:
     /** @var P\Application\SapiHttp|P\Application\SapiCli $application */
