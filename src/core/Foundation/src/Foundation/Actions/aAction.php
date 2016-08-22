089f271d43f1cbfa6b1f4e7ced6dc74ae1113afe
Payam Naderi <naderi.payam@gmail.com>
2016-08-22 11:35:55 +0430

    implement aAction neighbor actions and services access

--- src/core/Foundation/src/Foundation/Actions/aAction.php
+++ src/core/Foundation/src/Foundation/Actions/aAction.php
@@ -21,15 +21,63 @@ abstract class aAction
     /**
      * Call to neighbors module actions in container
      *
+     * note: for more readability all magic calls start with
+     *       upper case character.
+     *
+     * - call neighbor nested actions
+     *   $this->Authorize()
+     *
      * @param $method
      * @param $args
+     *
      * @return mixed
+     * @throws \Exception
      */
     function __call($method, $args)
     {
-        // TODO forward proxy call to invokablePlugins instantiate
+        # Nested Neighbor Actions
+        if ($this->services()->has($method)) {
+            // nested actions
+            $callable = $this->services()->get($method);
+            return call_user_func_array($callable, $args);
+        }
+
+        # Retrieve Nested Services
+        if ( substr($method, 0, strlen('Get')) === 'Get' && substr($method, -(strlen('Service'))) === 'Service' ) {
+            // Attain Module Nested Service
+            $service    = substr($method, strlen('Get'), strlen($method)-strlen('Service')-strlen('Get') );
+            $moduleName = $this->_getModuleName();
+            try {
+                $s = $this->services()->from("/module/{$moduleName}/services");
+                return $s->get($service);
+            } catch (\Exception $e) {
+                throw new \Exception(sprintf(
+                    'Error While Retrieve (%s) Service From Nested Module (%s).'
+                    , $service, $moduleName
+                ), 0, $e);
+            }
+        }
+
+
+        trigger_error('Call to undefined method '.__CLASS__.'::'.$method.'()', E_USER_ERROR);
     }
-    
+
+    /**
+     * 
+     * exp.
+     * GetModuleServices()->get('repository/clients')
+     * 
+     * @return false|Container
+     *
+     * @throws \Exception
+     */
+    function GetModuleServices()
+    {
+        $moduleName = $this->_getModuleName();
+        
+        $s = $this->services()->from("/module/{$moduleName}/services");
+        return $s;
+    }
     
     // Implement iCService
 
@@ -38,7 +86,7 @@ abstract class aAction
      *
      * - this actions also is member of Module Actions Container
      *
-     * @return Container
+     * @return ContainerForFeatureActions
      */
     function services()
     {
@@ -53,5 +101,17 @@ abstract class aAction
     function setServices(iContainer $container)
     {
         $this->services = $container;
+    }
+
+
+    // ..
+
+    function _getModuleName()
+    {
+        $path   = $this->services()->getPath();
+        $exPath = explode('/', $path);
+
+        $moduleName = $exPath[count($exPath)-2];
+        return $moduleName;
     }
 }
