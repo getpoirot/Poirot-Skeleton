5b5ead98c816462399d981c118675626a8712cfe
Payam Naderi <naderi.payam@gmail.com>
2016-09-03 17:11:06 +0430

    allow to change constants

--- index.consist.php
+++ index.consist.php
@@ -9,18 +9,18 @@
 define('PT_TIME_REQUEST_MICRO', microtime(true));
 
 $debug = getenv('PT_DEBUG');
-define('PT_DEBUG', ($debug && filter_var($debug, FILTER_VALIDATE_BOOLEAN)) ? $debug : false);
+define('PT_DEBUG', ($debug && filter_var($debug, FILTER_VALIDATE_BOOLEAN)) ? $debug : false, false);
 
-define('PT_DIR_WWW'          , dirname(__FILE__) );
-define('PT_DIR_THEME_DEFAULT', PT_DIR_WWW.'/theme' );
+define('PT_DIR_WWW'          , dirname(__FILE__), false);
+define('PT_DIR_THEME_DEFAULT', PT_DIR_WWW.'/theme', false);
 # by default application folder is in www public
 # it can be changed to any other folder like APP_DIR_WWW.'/../app-folder'
-define('PT_DIR_VENDOR',       PT_DIR_WWW.'/vendor');
-define('PT_DIR_SOURCE',       PT_DIR_WWW.'/src');
-define('PT_DIR_CORE',                PT_DIR_SOURCE.'/core');
-define('PT_DIR_CONFIG',              PT_DIR_SOURCE.'/config');
-define('PT_DIR_DATA',                PT_DIR_SOURCE.'/data');
-define('PT_DIR_TMP',                    PT_DIR_DATA.'/tmp');
+define('PT_DIR_VENDOR',       PT_DIR_WWW.'/vendor', false);
+define('PT_DIR_SOURCE',       PT_DIR_WWW.'/src', false);
+define('PT_DIR_CORE',                PT_DIR_SOURCE.'/core', false);
+define('PT_DIR_CONFIG',              PT_DIR_SOURCE.'/config', false);
+define('PT_DIR_DATA',                PT_DIR_SOURCE.'/data', false);
+define('PT_DIR_TMP',                    PT_DIR_DATA.'/tmp', false);
 
 // Setup autoLoading:
 if (file_exists(PT_DIR_VENDOR.'/autoload.php'))
