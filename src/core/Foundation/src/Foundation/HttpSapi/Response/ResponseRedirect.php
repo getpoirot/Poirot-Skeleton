d9106801510d6ec743e83c0c14d7c736a8f99724
Payam Naderi <naderi.payam@gmail.com>
2017-05-13 11:34:39 +0430

    fix integrate abstract http message

--- src/core/Foundation/src/Foundation/HttpSapi/Response/ResponseRedirect.php
+++ src/core/Foundation/src/Foundation/HttpSapi/Response/ResponseRedirect.php
@@ -3,13 +3,13 @@ namespace Module\Foundation\HttpSapi\Response;
 
 use Poirot\Http\HttpResponse;
 
-class ResponseRedirect extends HttpResponse
+
+class ResponseRedirect
+    extends HttpResponse
 {
     function __construct($uri, array $headers = array())
     {
         $headers['Location'] = (string) $uri;
         $this->setHeaders($headers);
-
-        parent::__construct();
     }
 }
