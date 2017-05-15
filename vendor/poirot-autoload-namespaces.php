bcb37a2d3e809b6d64cb01d76eda6777f9b6d164
Payam Naderi <naderi.payam@gmail.com>
2017-05-15 14:24:18 +0430

    improve autoload

--- vendor/poirot-autoload-namespaces.php
+++ vendor/poirot-autoload-namespaces.php
@@ -1,16 +1,5 @@
 <?php
+
 return array(
-    'Poirot\AaResponder'  => array(DIR_VENDOR.'/poirot/action-responder/Poirot/AaResponder'),
-    'Poirot\Application'  => array(DIR_VENDOR.'/poirot/application/Poirot/Application'),
-    'Poirot\Events'       => array(DIR_VENDOR.'/poirot/events/Poirot/Events'),
-    'Poirot\Http'         => array(DIR_VENDOR.'/poirot/http/Poirot/Http'),
-    'Poirot\Ioc'          => array(DIR_VENDOR.'/poirot/ioc/Poirot/Ioc'),
-    'Poirot\Loader'       => array(DIR_VENDOR.'/poirot/loader/Poirot/Loader'),
-    'Poirot\Logger'       => array(DIR_VENDOR.'/poirot/logger/Poirot/Logger'),
-    'Poirot\PathUri'      => array(DIR_VENDOR.'/poirot/pathuri/Poirot/PathUri'),
-    'Poirot\Psr7'         => array(DIR_VENDOR.'/poirot/psr7-http/Poirot/Psr7'),
-    'Poirot\Router'       => array(DIR_VENDOR.'/poirot/router/Poirot/Router'),
-    'Poirot\Std'          => array(DIR_VENDOR.'/poirot/std/Poirot/Std'),
-    'Poirot\Stream'       => array(DIR_VENDOR.'/poirot/stream/Poirot/Stream'),
-    'Poirot\View'         => array(DIR_VENDOR.'/poirot/view/Poirot/View'),
+    'Poirot\\Skeleton' => [ realpath(__DIR__.'/../src/'), ],
 );
--- vendor/poirot-autoload.php
+++ vendor/poirot-autoload.php
@@ -1,28 +1,22 @@
 <?php
 /* poirot-autoload.php @default poirot autoload */
 
-// Autoload Default Poirot Libraries:
-use Poirot\Loader\Autoloader\LoaderAutoloadClassMap;
-use Poirot\Loader\Autoloader\LoaderAutoloadNamespace;
+define('DIR_VENDOR', DIR_ROOT . '/vendor');
 
-// TODO using compiled cache autoload
 
 if (! class_exists('\Poirot\Loader\Autoloader\LoaderAutoloadAggregate') )
     // Used as base skeleton; it may also installed as composer package so the required packages is available.
-    require_once DIR_ROOT . '/vendor/poirot/loader' . '/Poirot/Loader/Autoloader/LoaderAutoloadAggregate.php';
+    require_once DIR_VENDOR. '/poirot/loader' . '/Poirot/Loader/Autoloader/LoaderAutoloadAggregate.php';
+
+
+// TODO using compiled cache autoload
 
 $namespaces = __DIR__.'/poirot-autoload-namespaces.php';
 $classmap   = __DIR__.'/poirot-autoload-classmap.php';
 
 $loader = new \Poirot\Loader\Autoloader\LoaderAutoloadAggregate(array(
-    'attach' => array(
-        array('loader' => new LoaderAutoloadClassMap,  'priority' => 100),
-        array('loader' => new LoaderAutoloadNamespace, 'priority' => 50),
-    ),
-    'Poirot\Loader\Autoloader\LoaderAutoloadClassMap'  => $namespaces,
-    #Poirot\Loader\Autoloader\LoaderAutoloadClassMap::class  => $namespaces,
-    'Poirot\Loader\Autoloader\LoaderAutoloadNamespace' => $classmap,
-    #Poirot\Loader\Autoloader\LoaderAutoloadNamespace::class => $classmap,
+    'Poirot\Loader\Autoloader\LoaderAutoloadClassMap'  => $classmap,
+    'Poirot\Loader\Autoloader\LoaderAutoloadNamespace' => $namespaces,
 ));
 
 $loader->register(true); // true for prepend loader in autoload stack
