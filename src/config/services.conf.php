c78572f5b1175316ef3e19d951b13153d7d0fe97
Payam Naderi <naderi.payam@gmail.com>
2017-07-02 15:30:36 +0430

    integrate container builder improvement

--- src/config/services.conf.php
+++ src/config/services.conf.php
@@ -17,33 +17,32 @@ return [
         ],
     'services' =>
         [
-            'sapi' =>
-            // Application Sapi Service Factory
-                [
-                    BuildContainer::INST => ServiceSapiApplication::class,
-                    'setting' => 'sapi.setting'
-                    // config can be (string) as registered service
-                    // or \Traversable|array instance
-                ],
-            'sapi.setting' =>
-            // Implement of \Traversable|array, defined as service so it can be
-            // replaced with other to load config from DB in exp.
-            // exp. load maintain modules if system is on maintain
-            //      or load specific modules for domain name, etc..
-                [
-                    BuildContainer::INST => ServiceSapiConfigDefault::class,
-                    'setting' => \Poirot\Std\catchIt(
-                        function () {
-                            $config = PT_DIR_CONFIG.'/sapi_default';
-                            if ($conf = \Poirot\Config\load($config))
-                                return $conf;
+            'sapi' => [
+                // Application Sapi Service Factory
 
-                            throw new \RuntimeException(sprintf(
-                                'Merged Config Named (%s) Has Error And Not Loaded.'
-                                , $config
-                            ));
-                        }
-                    ),
-                ],
+                ServiceSapiApplication::class,
+                'setting' => 'sapi.setting'
+                // config can be (string) as registered service
+                // or \Traversable|array instance
+            ],
+            'sapi.setting' => [
+                // Implement of \Traversable|array, defined as service so it can be
+                // replaced with other to load config from DB in exp.
+                // exp. load maintain modules if system is on maintain
+                //      or load specific modules for domain name, etc..
+                ServiceSapiConfigDefault::class,
+                'setting' => \Poirot\Std\catchIt(
+                    function () {
+                        $config = PT_DIR_CONFIG.'/sapi_default';
+                        if ($conf = \Poirot\Config\load($config))
+                            return $conf;
+
+                        throw new \RuntimeException(sprintf(
+                            'Merged Config Named (%s) Has Error And Not Loaded.'
+                            , $config
+                        ));
+                    }
+                ),
+            ],
         ],
 ];
